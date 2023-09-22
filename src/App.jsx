74bd050a62b1650194c5e316a3634fe47332c1aa
iserey <irina.serey@gmail.com>
2023-09-22 01:02:40 -0300

    funcionando header, todo lo demás comentado porque algo falla

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import './App.css'
+import './App.css';
+import Header from "./components/Header/Header";
+// import CardUser from "./components/CardUser/CardUser";
+// import NavBarMUI from './components/NavBarMUI/NavBarMUI';
+// import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 
-function App() {
-
+const App = () => {
   return (
     <div>
+      <Header/>
+
+      {/* <NavBarMUI /> */}
+
+      {/* <ItemListContainer /> */}
+
+      {/* <div className="UserSection">
+        <CardUser
+          name="Daniel Di Salvo"
+          date="Se unio en Enero 2021"
+          description="Scrum Master y UX Designer en Mercado Libre"
+          img="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        />
+
+        <CardUser
+          name="Raul Gutierrez"
+          date="Se unió en Abril 2021"
+          description="Scrum Master y UX Designer en Mercado Libre"
+          img="https://react.semantic-ui.com/images/avatar/large/elliot.jpg"
+        />
+
+        <CardUser
+          name="Francesca Bertotti"
+          date="Se unió en Julio 2021"
+          description="DevOps y profesora del curso de React.js en CoderHouse"
+          img="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
+        />
+
+      </div> */}
+
+      {/* <div className="CounterSection">
+        <p>{number}</p>
+      </div> */}
     </div>
   )
 }
 
-export default App
+export default App;
